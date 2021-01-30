b52b3f6e888f50812dd6485487264f18f9b2adc5
wookiehunter <70206518+wookiehunter@users.noreply.github.com>
2021-01-30 15:18:39 +0000

    User registration
    UI and alerts added

--- client/src/App.js
+++ client/src/App.js
@@ -4,24 +4,35 @@ import NavBar from './components/layout/NavBar'
 import Home from './components/pages/Home'
 import About from './components/pages/About'
 import ContactState from './context/contact/ContactState'
+import AuthState from './context/auth/AuthState'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+import AlertState from './context/alert/AlertState'
+import Alerts from './components/layout/Alerts'
 import './App.css';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <NavBar />
-          <div className="container">
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
-
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <NavBar />
+              <div className="container">
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   )
 }
 
